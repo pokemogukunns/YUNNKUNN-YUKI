ecd1be4e2277f0bdd8af79526644b800a50e1f49
pokemogukunns <nattyannkunndesu@icloud.com>
2024-09-17 11:11:08 +0900

    Create youtube_to_mp3.py

---
+++ youtube_to_mp3.py
@@ -0,0 +1,19 @@
+import yt_dlp
+
+def download_youtube_as_mp3(url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'outtmpl': '%(title)s.%(ext)s',  # ファイル名のテンプレート
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+if __name__ == "__main__":
+    youtube_url = input("YouTubeのURLを入力してください: ")
+    download_youtube_as_mp3(youtube_url)
